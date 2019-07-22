e172de6a4bccb2925a433f44519866729f81ecf3
An <an.chau@pops.vn>
2019-07-22 16:17:50 +0700

    add props to focusablesection

--- build/index.js
+++ build/index.js
@@ -1669,7 +1669,9 @@ var SpatialNavigation = function (_Component) {
       }
 
       // Set defaultElement
+      console.log(30, this.props.defaultElement);
       if (typeof this.props.defaultElement === 'string') {
+        console.log(31, this.props.defaultElement);
         propsConfig.defaultElement = this.props.defaultElement;
       }
 
@@ -1684,7 +1686,9 @@ var SpatialNavigation = function (_Component) {
       }
 
       // Set leaveFor
+      console.log(20, this.props.leaveFor);
       if (_typeof(this.props.leaveFor) === 'object') {
+        console.log(21, this.props.leaveFor);
         propsConfig.leaveFor = this.props.leaveFor;
       }
 
@@ -1733,6 +1737,7 @@ var SpatialNavigation = function (_Component) {
   }, {
     key: 'componentDidMount',
     value: function componentDidMount() {
+      console.log(40, this.props);
       if (!this.props.customInit) {
         _spatial_navigation2.default.init();
         _spatial_navigation2.default.add(config);
@@ -1939,10 +1944,17 @@ var FocusableSection = function (_Component3) {
         defaultElement = this._getSelector() + ('.' + config.activeClassName);
       }
 
+      var straightOnly = this.props.straightOnly,
+          leaveFor = this.props.leaveFor,
+          restrict = this.props.restrict;
+
       _spatial_navigation2.default.set(this.sectionId, {
         selector: this._getSelector(),
         enterTo: enterTo,
-        defaultElement: defaultElement
+        defaultElement: defaultElement,
+        straightOnly: straightOnly,
+        leaveFor: leaveFor,
+        restrict: restrict
       });
     }
   }, {
